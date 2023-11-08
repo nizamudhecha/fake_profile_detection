b0eab08bbaeec8861e7bd252264541f24235ef70
nizamudhecha <139213653+nizamudhecha@users.noreply.github.com>
2023-11-08 18:10:22 +0530

    Delete fpd directory

--- fpd/fpd/s.py
+++
@@ -1,5 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-
-def create_placeholder_model():
-    model = RandomForestClassifier(n_estimators=100, random_state=42)  # Placeholder model
-    return model
--- fpd/fpd1/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Define your Django model
-class MyModel(models.Model):
-    json_data = models.JSONField()
-
-# Create your models here.
